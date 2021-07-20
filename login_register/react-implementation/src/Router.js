cd950c4510d4dc929870fd6c85730651b012141c
howard0923 <75877433+howard0923@users.noreply.github.com>
2021-07-20 22:31:59 +0800

    7/20

--- login_register/react-implementation/src/Router.js
+++ login_register/react-implementation/src/Router.js
@@ -7,16 +7,12 @@ import NotFound from 'pages/NotFound';
 import Cart from 'pages/Cart';
 import Contact from 'pages/Contact';
 import First from 'pages/First';
-<<<<<<< HEAD
 import Member from 'pages/Member';
 import Detail from 'components/Transaction/Detail';
-
-=======
 import AboutUs from 'pages/AboutUs';
 import FAQ from 'pages/FAQ';
 import Guide from 'pages/Guide';
 import Rules from 'pages/Rules';
->>>>>>> 50047671186aacb4ee30fa3b3f0494699ef3ae58
 
 const Router = () => (
   <BrowserRouter>
@@ -27,16 +23,12 @@ const Router = () => (
       <Route path="/cart" component={Cart} />
       <Route path="/contact" component={Contact} />
       <Route path="/first" component={First} />
-<<<<<<< HEAD
       <Route path="/member" component={Member} />
       <Route path="/detail" component={Detail} />
-      
-=======
       <Route path="/aboutus" component={AboutUs} />
       <Route path="/FAQ" component={FAQ} />
       <Route path="/guide" component={Guide} />
       <Route path="/rules" component={Rules} />
->>>>>>> 50047671186aacb4ee30fa3b3f0494699ef3ae58
       <Route component={NotFound} />
     </Switch>
   </BrowserRouter>
