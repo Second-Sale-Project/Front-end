d7c532fce3bb499bd28e674da1706f5ff04e1df2
hummingsinger <poyengood13@gmail.com>
2021-09-13 16:51:21 +0800

    0913

--- login_register/react-implementation/src/pages/AdminOrder.js
+++ login_register/react-implementation/src/pages/AdminOrder.js
@@ -8,17 +8,17 @@ export default function AdminOrder(props) {
         <section class="w80per marginlrauto">
             <div className="w30per textcenter fontbold">訂單編號</div>
             <div className="w100per h30px positionrelative verticalcenter martb30px">
-               <div className="w30per textcenter fontbold fontsmall positionabsolute left0per">A111111111</div>
+               <div className="w30per textcenter fontbold positionabsolute left0per">A111111111</div>
                <button className="orderbutton fontbold positionabsolute right30per">出貨</button>
                <button className="orderbutton fontbold positionabsolute right0per">收貨</button>
             </div>
             <div className="w100per h30px positionrelative verticalcenter martb30px">
-               <div className="w30per textcenter fontbold fontsmall positionabsolute left0per">A111111111</div>
+               <div className="w30per textcenter fontbold positionabsolute left0per">A111111111</div>
                <button className="orderbutton fontbold positionabsolute right30per">出貨</button>
                <button className="orderbutton fontbold positionabsolute right0per">收貨</button>
             </div>
             <div className="w100per h30px positionrelative verticalcenter martb30px">
-               <div className="w30per textcenter fontbold fontsmall positionabsolute left0per">A111111111</div>
+               <div className="w30per textcenter fontbold positionabsolute left0per">A111111111</div>
                <button className="orderbutton fontbold positionabsolute right30per">出貨</button>
                <button className="orderbutton fontbold positionabsolute right0per">收貨</button>
             </div>
