0a990a14e308267af5777658a7688dac44aabd15
Charming0420 <tommy20326999@gmail.com>
2021-10-21 20:30:14 +0800

    sidebar connect sql with color section
    可更改顏色喜好成功

--- login_register/react-implementation/src/components/SideBar.js
+++ login_register/react-implementation/src/components/SideBar.js
@@ -24,8 +24,230 @@ import type8 from "../images/img/type8.jpg"
 import type9 from "../images/img/type9.jpg"
 import type10 from "../images/img/type10.png"
 import type12 from "../images/img/type12.png"
+import axios from "commons/axios"
 
 function SideBar(props) {
+  //-----------------------color function---------------------------
+  function LikeColorgrey() {
+    const likeColor = "grey"
+    const user = global.auth.getUser() || {}
+    const email = user.email
+    axios
+      .post(`http://140.117.71.141:3001/api/LikeColorChange`, {
+        likeColor,
+        email,
+      })
+      .then((res) => {
+        console.log(res)
+        if (document.getElementById("grey").style.borderStyle === "solid") {
+          document.getElementById("grey").style.borderStyle = "none"
+          axios.post(`http://140.117.71.141:3001/api/LikeColorDelete`, {
+            likeColor,
+            email,
+          })
+        } else {
+          document.getElementById("grey").style.borderStyle = "solid"
+        }
+      })
+  }
+  function LikeColoryellow() {
+    const likeColor = "yellow"
+    const user = global.auth.getUser() || {}
+    const email = user.email
+    axios
+      .post(`http://140.117.71.141:3001/api/LikeColorChange`, {
+        likeColor,
+        email,
+      })
+      .then((res) => {
+        console.log(res)
+        if (document.getElementById("yellow").style.borderStyle === "solid") {
+          document.getElementById("yellow").style.borderStyle = "none"
+          axios.post(`http://140.117.71.141:3001/api/LikeColorDelete`, {
+            likeColor,
+            email,
+          })
+        } else {
+          document.getElementById("yellow").style.borderStyle = "solid"
+        }
+      })
+  }
+  function LikeColorRed() {
+    const likeColor = "red"
+    const user = global.auth.getUser() || {}
+    const email = user.email
+    axios
+      .post(`http://140.117.71.141:3001/api/LikeColorChange`, {
+        likeColor,
+        email,
+      })
+      .then((res) => {
+        console.log(res)
+        if (document.getElementById("red").style.borderStyle === "solid") {
+          document.getElementById("red").style.borderStyle = "none"
+          axios.post(`http://140.117.71.141:3001/api/LikeColorDelete`, {
+            likeColor,
+            email,
+          })
+        } else {
+          document.getElementById("red").style.borderStyle = "solid"
+        }
+      })
+  }
+  function LikeColorlittlepink() {
+    const likeColor = "littlepink"
+    const user = global.auth.getUser() || {}
+    const email = user.email
+    axios
+      .post(`http://140.117.71.141:3001/api/LikeColorChange`, {
+        likeColor,
+        email,
+      })
+      .then((res) => {
+        console.log(res)
+        if (document.getElementById("littlepink").style.borderStyle === "solid") {
+          document.getElementById("littlepink").style.borderStyle = "none"
+          axios.post(`http://140.117.71.141:3001/api/LikeColorDelete`, {
+            likeColor,
+            email,
+          })
+        } else {
+          document.getElementById("littlepink").style.borderStyle = "solid"
+        }
+      })
+  }
+  function LikeColoryellowwhite() {
+    const likeColor = "yellowwhite"
+    const user = global.auth.getUser() || {}
+    const email = user.email
+    axios
+      .post(`http://140.117.71.141:3001/api/LikeColorChange`, {
+        likeColor,
+        email,
+      })
+      .then((res) => {
+        console.log(res)
+        if (document.getElementById("yellowwhite").style.borderStyle === "solid") {
+          document.getElementById("yellowwhite").style.borderStyle = "none"
+          axios.post(`http://140.117.71.141:3001/api/LikeColorDelete`, {
+            likeColor,
+            email,
+          })
+        } else {
+          document.getElementById("yellowwhite").style.borderStyle = "solid"
+        }
+      })
+  }
+  function LikeColorlittleblue() {
+    const likeColor = "littleblue"
+    const user = global.auth.getUser() || {}
+    const email = user.email
+    axios
+      .post(`http://140.117.71.141:3001/api/LikeColorChange`, {
+        likeColor,
+        email,
+      })
+      .then((res) => {
+        console.log(res)
+        if (document.getElementById("littleblue").style.borderStyle === "solid") {
+          document.getElementById("littleblue").style.borderStyle = "none"
+          axios.post(`http://140.117.71.141:3001/api/LikeColorDelete`, {
+            likeColor,
+            email,
+          })
+        } else {
+          document.getElementById("littleblue").style.borderStyle = "solid"
+        }
+      })
+  }
+  function LikeColorbrown() {
+    const likeColor = "brown"
+    const user = global.auth.getUser() || {}
+    const email = user.email
+    axios
+      .post(`http://140.117.71.141:3001/api/LikeColorChange`, {
+        likeColor,
+        email,
+      })
+      .then((res) => {
+        console.log(res)
+        if (document.getElementById("brown").style.borderStyle === "solid") {
+          document.getElementById("brown").style.borderStyle = "none"
+          axios.post(`http://140.117.71.141:3001/api/LikeColorDelete`, {
+            likeColor,
+            email,
+          })
+        } else {
+          document.getElementById("brown").style.borderStyle = "solid"
+        }
+      })
+  }
+  function LikeColorblack() {
+    const likeColor = "black"
+    const user = global.auth.getUser() || {}
+    const email = user.email
+    axios
+      .post(`http://140.117.71.141:3001/api/LikeColorChange`, {
+        likeColor,
+        email,
+      })
+      .then((res) => {
+        console.log(res)
+        if (document.getElementById("black").style.borderStyle === "solid") {
+          document.getElementById("black").style.borderStyle = "none"
+          axios.post(`http://140.117.71.141:3001/api/LikeColorDelete`, {
+            likeColor,
+            email,
+          })
+        } else {
+          document.getElementById("black").style.borderStyle = "solid"
+        }
+      })
+  }
+  function LikeColorwhite() {
+    const likeColor = "white"
+    const user = global.auth.getUser() || {}
+    const email = user.email
+    axios
+      .post(`http://140.117.71.141:3001/api/LikeColorChange`, {
+        likeColor,
+        email,
+      })
+      .then((res) => {
+        console.log(res)
+        if (document.getElementById("white").style.borderStyle === "solid") {
+          document.getElementById("white").style.borderStyle = "none"
+          axios.post(`http://140.117.71.141:3001/api/LikeColorDelete`, {
+            likeColor,
+            email,
+          })
+        } else {
+          document.getElementById("white").style.borderStyle = "solid"
+        }
+      })
+  }
+  function LikeColorpink() {
+    const likeColor = "pink"
+    const user = global.auth.getUser() || {}
+    const email = user.email
+    axios
+      .post(`http://140.117.71.141:3001/api/LikeColorChange`, {
+        likeColor,
+        email,
+      })
+      .then((res) => {
+        console.log(res)
+        if (document.getElementById("pink").style.borderStyle === "solid") {
+          document.getElementById("pink").style.borderStyle = "none"
+          axios.post(`http://140.117.71.141:3001/api/LikeColorDelete`, {
+            likeColor,
+            email,
+          })
+        } else {
+          document.getElementById("pink").style.borderStyle = "solid"
+        }
+      })
+  }
   return (
     <div className={`sidebar-menu${props.isMenuOpen === true ? " open" : ""}`}>
       <div className="columns is-mobile">
@@ -52,16 +274,16 @@ function SideBar(props) {
       <div class="link-top"></div>
       <div className="sidebarmiddle">
         <div className="sidebarmiddlefont">顏色</div>
-        <img src={red} className="colorCircle"></img>
-        <img src={littlepink} className="colorCircle"></img>
-        <img src={pink} className="colorCircle"></img>
-        <img src={brown} className="colorCircle"></img>
-        <img src={yellow} className="colorCircle"></img>
-        <img src={yellowwhite} className="colorCircle"></img>
-        <img src={littleblue} className="colorCircle"></img>
-        <img src={grey} className="colorCircle"></img>
-        <img src={white} className="colorCircle"></img>
-        <img src={black} className="colorCircle"></img>
+        <img id="red" src={red} className="colorCircle" onClick={LikeColorRed}></img>
+        <img id="littlepink" src={littlepink} className="colorCircle" onClick={LikeColorlittlepink}></img>
+        <img id="pink" src={pink} className="colorCircle" onClick={LikeColorpink}></img>
+        <img id="brown" src={brown} className="colorCircle" onClick={LikeColorbrown}></img>
+        <img id="yellow" src={yellow} className="colorCircle" onClick={LikeColoryellow}></img>
+        <img id="yellowwhite" src={yellowwhite} className="colorCircle" onClick={LikeColoryellowwhite}></img>
+        <img id="littleblue" src={littleblue} className="colorCircle" onClick={LikeColorlittleblue}></img>
+        <img id="grey" src={grey} className="colorCircle" onClick={LikeColorgrey}></img>
+        <img id="white" src={white} className="colorCircle" onClick={LikeColorwhite}></img>
+        <img id="black" src={black} className="colorCircle" onClick={LikeColorblack}></img>
         <hr></hr>
         <div className="sidebarmiddlefont">包型</div>
         <img src={type1} className="colorCircle"></img>
