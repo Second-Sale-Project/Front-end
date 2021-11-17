e6750cc21b9e90166c56fb143ba5990dc2095d0b
www0920tw <48194622+www0920tw@users.noreply.github.com>
2021-11-17 21:22:51 +0800

    211117 修改頁面內容

--- login_register/react-implementation/src/pages/FAQ.js
+++ login_register/react-implementation/src/pages/FAQ.js
@@ -17,20 +17,14 @@ const FAQ = (props) => {
                         </div>
                         {/* <div class="fontlarge marginlrauto"> */}
                             <table class="has-text-left fontlarge marginlrauto w95per">
-                                <thead>
-                                    <tr>
-                                        <td class="pl-2 has-text-weight-bold tableborder1 tablepad1">Line客服</td>
-                                        <td class="tableborder1 tablepad2">@id</td>
-                                    </tr>
-                                </thead>
                                 <tbody>
                                     <tr>
-                                        <td class="pl-2 has-text-weight-bold tableborder1 tablepad1">Fb粉絲專頁</td>
-                                        <td class="tableborder1 tablepad2">二手精品訂閱</td>
+                                        <td class="pl-2 has-text-weight-bold tableborder1 tablepad1">問：平台可以使用哪些付款方式？</td>
+                                        <td class="tableborder1 tablepad2">答：目前提供信用卡線上刷卡的付款方式。</td>
                                     </tr>
                                     <tr>
-                                        <td class="pl-2 has-text-weight-bold tableborder2 tablepad1">客服信箱</td>
-                                        <td class="tableborder2 tablepad2">service@mail.com</td>
+                                        <td class="pl-2 has-text-weight-bold tableborder2 tablepad1">問：忘記密碼該怎麼辦?</td>
+                                        <td class="tableborder2 tablepad2">答：可於登入頁面中點選【忘記密碼】按鈕，進行後續系統指示之操作來重設密碼。</td>
                                     </tr>
                                 </tbody>
                             </table>
